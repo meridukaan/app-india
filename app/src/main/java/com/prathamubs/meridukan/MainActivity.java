3b6ded35fd80271906cdf932c18181c7fbfdd7e5
Ankur Badola <ankurbadola@gmail.com>
2018-08-30 21:31:19 +0530

    Enable webview

--- app/src/main/java/com/prathamubs/meridukan/MainActivity.java
+++ app/src/main/java/com/prathamubs/meridukan/MainActivity.java
@@ -2,12 +2,20 @@ package com.prathamubs.meridukan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("file:///android_asset/www/index.html");
     }
 }
