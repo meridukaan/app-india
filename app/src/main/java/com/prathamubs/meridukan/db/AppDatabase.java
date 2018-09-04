6270a05fb5d2980894363375dea9f7807958df1b
Ankur Badola <ankurbadola@gmail.com>
2018-09-04 19:07:23 +0530

    Add missing extension to db file

--- app/src/main/java/com/prathamubs/meridukan/db/AppDatabase.java
+++ app/src/main/java/com/prathamubs/meridukan/db/AppDatabase.java
@@ -17,7 +17,7 @@ public abstract class AppDatabase extends RoomDatabase {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context, AppDatabase.class,
-                            "PrathamTabDB").build();
+                            "PrathamTabDB.db").build();
                 }
             }
         }
