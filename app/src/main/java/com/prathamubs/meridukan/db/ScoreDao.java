3060485db5e69503255aa6d42a48e6a1bb8900f8
Ankur Badola <ankurbadola@gmail.com>
2018-09-05 17:54:18 +0530

    Replace entry when doing same insert

--- app/src/main/java/com/prathamubs/meridukan/db/ScoreDao.java
+++ app/src/main/java/com/prathamubs/meridukan/db/ScoreDao.java
@@ -2,13 +2,14 @@ package com.prathamubs.meridukan.db;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
 
 @Dao
 public interface ScoreDao extends Insertable<Score> {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Score score);
 
     @Query("SELECT * from Scores")
--- app/src/main/java/com/prathamubs/meridukan/db/StudentDao.java
+++ app/src/main/java/com/prathamubs/meridukan/db/StudentDao.java
@@ -2,13 +2,14 @@ package com.prathamubs.meridukan.db;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
 
 @Dao
 public interface StudentDao extends Insertable<Student> {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Student student);
 
     @Query("SELECT * from Student")
