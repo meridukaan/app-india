5cb3c567d81470b269edb9151bdbe2ae2047a5e8
Ankur Badola <ankurbadola@gmail.com>
2019-02-05 08:57:53 +0530

    Remove subtree to add again

--- app/src/main/assets/www/README.md
+++
@@ -1,3 +0,0 @@
-# prathamubs
-Pratham Initiative taken by UBS
-This is Pratham POC code. We will integrate it later with actual pratham code base.
--- app/src/main/assets/www/js/audioMain.js
+++
@@ -1,9 +0,0 @@
-const playAudio = function(divElement){
-	if(divElement !=null)
-	{	
-		divElement.addEventListener('playAudio', function (e) {
-	    	$("#soundtrack").attr("src", e.detail);
-			document.getElementById('soundtrack').play();
-		});
-	}
-}
